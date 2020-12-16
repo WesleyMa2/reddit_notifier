fd48c6922329a8d4936324d3e44894d3665c1f06
Wesley <weselykhma@gmail.com>
2020-12-16 18:23:21 -0500

    improved polling effiency and cli

--- app.js
+++ app.js
@@ -2,81 +2,93 @@ let util = require('util');
 let https = require('https');
 
 const notifier = require('node-notifier');
+const { prototype } = require('stream');
 
 
 let SUBREDDIT = 'bapcsalescanada';
 let INTERVAL = 60000;
 let filter = function (title) { return true };
-let flagOffset = 0;
+let argsCount = 2;
 
 // Set include/exlude filters
 const includeFlag = process.argv.indexOf('-i');
 const excludeFlag = process.argv.indexOf('-e');
+const intervalFlag = process.argv.indexOf('-t');
 if (includeFlag > -1) {
     filter = function (title) { return new RegExp(process.argv[includeFlag + 1], 'gi').test(title) }
-    flagOffset += 2;
-} else if (excludeFlag > -1) {
+    argsCount += 2;
+}
+if (excludeFlag > -1) {
     filter = function (title) { return !(new RegExp(process.argv[excludeFlag + 1], 'gi').test(title)) }
-    flagOffset += 2;
+    argsCount += 2;
+}
+if (intervalFlag > -1) {
+    INTERVAL = process.argv[1 + intervalFlag] * 1000;
+    argsCount += 2;
 }
 
-// Set subbreddit
-if (process.argv.length >= 3 + flagOffset) SUBREDDIT = process.argv[2 + flagOffset];
-// Set polling interval (Seconds)
-if (process.argv.length >= 4 + flagOffset) INTERVAL = process.argv[3 + flagOffset] * 1000;
+// Set subbreddit;
+if (argsCount > 2 && process.argv.length > argsCount + 1 ||
+    process.argv.length > argsCount) SUBREDDIT = process.argv[process.argv.length-1];
 
 
-const URL = util.format('https://www.reddit.com/r/%s/new.json', SUBREDDIT);
+let url = util.format('https://www.reddit.com/r/%s/new.json', SUBREDDIT);
 
-let lastTimeStamp = new Date(0);
-const localTZoffset = lastTimeStamp.getTimezoneOffset() / 60 + 3; // too lazy to make this part good
+let latestPost;
+// const localTZoffset = latestPost.getTimezoneOffset() / 60 + 3; // too lazy to make this part good
 function dateToString(timestamp, useLocal) {
     let dateObj = new Date((timestamp));
     if (useLocal) {
-        dateObj.setHours(dateObj.getHours() - localTZoffset);
+        dateObj.setHours(dateObj.getHours() - 8);
     }
     return dateObj.toLocaleDateString() + ' - ' + dateObj.toLocaleTimeString();
 }
 
 function pollReddit() {
     // console.log('Last post- ' + dateToString(lastTimeStamp, true))
-    https.get(URL, (resp) => {
+    https.get(url, (resp) => {
         let rawJson = '';
-        let latestPosts = null;
-        // A chunk of data has been recieved.
+        let json;
+        let postsArr = null;
         resp.on('data', (chunk) => {
             rawJson += chunk;
         });
-        // The whole response has been received.
+
         resp.on('end', () => {
-            latestPosts = JSON.parse(rawJson).data.children;
-            const firstPost = latestPosts[0].data;
-            const firstPostTimestamp = new Date(firstPost.created * 1000);
-            // On first poll, set last timestamp to latest post
-            if (lastTimeStamp.getTime() === 0) {
-                lastTimeStamp = firstPostTimestamp;
+            json = JSON.parse(rawJson);
+            if(!json.data) {
+                console.log("ERROR! Recieved the following instead:")
+                console.log(json);
                 return;
             }
-            // On subsequent polls, print out a message for each post newer than the last saved timestamp
-            // and pass the filter
-            let count = 0;
-            for (let i = 0; i < latestPosts.length; i++) {
-                const post = latestPosts[i];
-                let postTimestamp = post.data.created * 1000;
-                const postTitle = post.data.title;
-                if (postTimestamp > lastTimeStamp && filter(postTitle)) {
-                    count++
-                    const postLink = post.data.url_overridden_by_dest;
+            postsArr = JSON.parse(rawJson).data.children;
+            const firstPost = postsArr[0] ? postsArr[0].data : null;
+            // On first poll, set last timestamp to latest post
+            if (!latestPost) {
+                latestPost = firstPost.name;
+                url = util.format('https://www.reddit.com/r/%s/new.json?before=%s', SUBREDDIT, latestPost);
+                return;
+            }
+            // On subsequent polls, print out a message for each post newer than the last seen post
+            // and passes the filter
+            // console.log(postsArr.length + ' posts since ' + latestPost);
+            for (let i = postsArr.length; i > 0; i--) {
+                const post = postsArr[i-1].data;
+                const postTimestamp = post.created * 1000;
+                const postTitle = post.title;
+                const postLink = post.url_overridden_by_dest;
+                if (filter(postTitle)) {
                     console.log(util.format('\n[%s]\t%s', dateToString(postTimestamp, true), postTitle));
                     console.log('\t\t\t\t' + postLink);
-                } else { break; }
+                }
             }
             // Update latest timestamp, push notification for latest post
-            if (count > 0) {
-                lastTimeStamp = firstPostTimestamp;
+            if (postsArr.length > 0) {
+                latestPost = firstPost.name;
+                url = util.format('https://www.reddit.com/r/%s/new.json?before=%s', SUBREDDIT, latestPost);
                 notifier.notify(
                     {
-                        title: count + (count === 25 ? '+' : '') + ' new posts!',
+                        title: postsArr.length + (postsArr.length === 25 ? '+' : '') + ' new posts!',
                         message: firstPost.title,
                         sound: false,
                         wait: true,
@@ -90,7 +102,9 @@ function pollReddit() {
         console.log("Error: " + err.message);
     });
 }
-console.log(util.format('Checking [%s] for new posts every %i seconds', SUBREDDIT, INTERVAL / 1000));
+console.log(util.format('Checking r/%s for new posts every %i seconds', SUBREDDIT, INTERVAL / 1000));
+if (excludeFlag > -1) console.log('Excluding all posts that match given regex');
+else if (includeFlag > -1) console.log('Including only posts that match given regex');
 console.log('##########################################################');
 setInterval(pollReddit, INTERVAL);
 pollReddit()
